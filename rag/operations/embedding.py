ae9d31d43105a9af65edb18d7a28c43092d19741
ShawarmaOnGit <irahmanworks@gmail.com>
2026-08-17 15:30:04 -0400

    feat: Finish first draft of embeddings.py

--- rag/operations/embedding.py
+++ rag/operations/embedding.py
@@ -6,29 +6,39 @@ from dotenv import load_dotenv
 
 load_dotenv()
 client = OpenAI()
+EMBEDDING_MODEL = "text-embedding-3-small"
+
 
 def embed_ingestion(chunks: list[Chunk]) -> np.ndarray:
     response = client.embeddings.create(
-        model="text-embedding-3-small",
+        model=EMBEDDING_MODEL,
         input=[chunk.text for chunk in chunks]
     )
 
     embeddings = np.array([item.embedding for item in response.data])
 
-    assert len(response.data) == len(chunks), "There is a mismatch between chunks and embeddings"
+    assert len(response.data) == len(chunks), \
+        "There is a mismatch between chunks and embeddings"
 
     for i, item in enumerate(response.data):
-        assert item.index == i, "Chunks and embeddings order do not match"
+        assert item.index == i, \
+            "Chunks and embeddings order do not match"
 
-    assert embeddings.shape == (len(chunks), 1536), f"Embeddings is not in the right shape. Current shape: {embeddings.shape}"
+    assert embeddings.shape == (len(chunks), 1536), \
+        f"Embeddings is not in the right shape. Current shape: {embeddings.shape}"
 
     return embeddings
 
 
 def embed_query(query: str) -> np.ndarray:
     response = client.embeddings.create(
-        model="text-embedding-3-small",
+        model=EMBEDDING_MODEL,
         input=query
     )
 
+    embedding = np.array(response.data[0].embedding)
 
+    assert embedding.shape == (1536,), \
+        f"Embedding is not in the right shape. Current shape: {embedding.shape}"
+
+    return embedding
