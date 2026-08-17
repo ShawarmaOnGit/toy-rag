88f065ceb3929b29b2c5bc29dd717d5def82a044
ShawarmaOnGit <irahmanworks@gmail.com>
2026-08-17 15:25:57 -0400

    feat: Add shape assertion in embed_ingestion

--- rag/operations/embedding.py
+++ rag/operations/embedding.py
@@ -13,13 +13,22 @@ def embed_ingestion(chunks: list[Chunk]) -> np.ndarray:
         input=[chunk.text for chunk in chunks]
     )
 
+    embeddings = np.array([item.embedding for item in response.data])
+
     assert len(response.data) == len(chunks), "There is a mismatch between chunks and embeddings"
 
     for i, item in enumerate(response.data):
         assert item.index == i, "Chunks and embeddings order do not match"
 
-    return np.array([item.embedding for item in response.data])
+    assert embeddings.shape == (len(chunks), 1536), f"Embeddings is not in the right shape. Current shape: {embeddings.shape}"
+
+    return embeddings
 
 
 def embed_query(query: str) -> np.ndarray:
-    pass
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=query
+    )
+
+
